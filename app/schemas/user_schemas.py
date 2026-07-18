38f2f61ac1cca9ae2e2cefab9bb0594b168efa04
mabuujin <ruslanyu71@gmail.com>
2026-07-18 22:08:50 +0300

    email is now EmailStr and id added to the UserPublic class

--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -1,21 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from app.models import UserRole
 
 class UserCreate(BaseModel):
     name: str
     password: str
-    email: str
+    email: EmailStr
     role: UserRole = UserRole.customer
 
 class UserPublic(BaseModel):
+    id: int
     name: str
-    email: str
+    email: EmailStr
     role: UserRole
 
 # class UserPrivate(UserPublic):
 #     password_hash: str
 
 class UserEdit(BaseModel):
+    id: int
     name: str | None = None
-    password: str | None = None
     email: str | None = None
